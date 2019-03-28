753b200d3d2b897ee3d1088013add40d20e8797a
Livdroid <oliviarodrigue@gmail.com>
2019-03-28 17:50:46 +0100

    Update BaseView.kt

--- app/src/main/java/dimsuncom/mvpsample/BaseView.kt
+++ app/src/main/java/dimsuncom/mvpsample/BaseView.kt
@@ -1,14 +1,11 @@
 package dimsuncom.mvpsample
 
-
 /*
  * Base View for all the app
  * Fill with methods you want to implement generally in all subViws
  *
  */
 interface BaseView {
-
     fun showLoading()
-
     fun hideLoading()
 }
